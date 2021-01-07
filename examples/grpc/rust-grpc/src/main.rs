e1dae71632bdc0230134b30f080e91b169c50801
joham <mail@joham.dev>
2021-01-07 21:42:55 +0100

    NoteService

--- examples/grpc/rust-grpc/src/main.rs
+++ examples/grpc/rust-grpc/src/main.rs
@@ -1,8 +1,45 @@
+use std::collections::HashMap;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::time::Instant;
+
+use futures_core::Stream;
+use tokio::sync::mpsc;
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+
+use notes::notes_example_server::{NotesExample, NotesExampleServer};
+use notes::{Note, Author, Title};
+
+pub mod notes {
+    // You need to pass the name of the package declared in the .proto file here. In this case: notes.
+    tonic::include_proto!("notes");
+}
+
 #[derive(Debug)]
 struct NoteService;
 
-pub mod notes {
-    tonic::include_proto!("notes");
+#[tonic::async_trait]
+impl NotesExample for NoteService {
+    async fn get_note_by_title(&self, _request: Request<Title>) -> Result<Response<Note>, Status> {
+        unimplemented!()
+    }
+
+    type ListNotesByAuthorStream = mpsc::Receiver<Result<Note, Status>>;
+
+    async fn list_notes_by_author(
+        &self,
+        _request: Request<Author>,
+    ) -> Result<Response<Self::ListNotesByAuthorStream>, Status> {
+        unimplemented!()
+    }
+
+    async fn add_notes(
+        &self,
+        _request: Request<tonic::Streaming<Note>>,
+    ) -> Result<Response<()>, Status> {
+        unimplemented!()
+    }
 }
 
 fn main() {
