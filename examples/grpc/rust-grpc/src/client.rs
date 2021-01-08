11c518f438abbdd560aafbe753f6826a386512bb
joham <mail@joham.dev>
2021-01-08 15:55:04 +0100

    client

---
+++ examples/grpc/rust-grpc/src/client.rs
@@ -0,0 +1,44 @@
+pub mod notes {
+    tonic::include_proto!("notes");
+}
+
+use notes::noticeboard_client::NoticeboardClient;
+use notes::{Author, Note, Title};
+use tonic::Request;
+use tonic::transport::Channel;
+use std::error::Error;
+
+async fn get_notes_by_author(
+    client: &mut NoticeboardClient<Channel>,
+) -> Result<(), Box<dyn Error>> {
+    let author = Author {
+        nickname: "Hans".to_string(),
+        mail: "hans@gmail.com".to_string(),
+    };
+
+    let mut stream = client
+        .list_notes_by_author(Request::new(author))
+        .await?
+        .into_inner();
+
+    while let Some(note) = stream.message().await? {
+        println!("NOTE = {:?}", note);
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = NoticeboardClient::connect("http://[::1]:10000").await?;
+
+    let response = client
+        .get_note_by_title(Request::new(Title {
+            title: "Hello".to_string(),
+        }))
+        .await?;
+
+    println!("RESPONSE = {:?}", response);
+
+    Ok(())
+}
