7a0bf4b76cadc0ccd778a24654fb2ce449e7e093
joham <mail@joham.dev>
2021-01-10 01:48:59 +0100

    go client

---
+++ grpc-example/go-grpc/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	noticeboard "./build/gen"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// connect to the server
+	srv, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("can not connect with server %v", err)
+	}
+
+	client := noticeboard.NewNoticeboardClient(srv)
+	author := &noticeboard.Author{Nickname: "", Mail: "hans@gmail.com"}
+	// open the stream
+	stream, err := client.ListNotesByAuthor(context.Background(), author)
+	if err != nil {
+		log.Fatalf("open stream error %v", err)
+	}
+
+	ch := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				ch <- true 
+				return
+			}
+			if err != nil {
+				log.Fatalf("cannot receive %v", err)
+			}
+			log.Printf("Resp received: %s", resp.Content)
+		}
+	}()
+
+	<-ch //we will wait until all response is received
+	log.Printf("finished")
+}
